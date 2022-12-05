0460472c62cf02f156b22bb2d2b8d57d8d8978ff
Vinayak Raina <115440143+RAINAVINAYAK16@users.noreply.github.com>
2022-12-05 08:59:11 +0530

    Update GitHub_test_file.py

--- GitHub_test_file.py
+++ GitHub_test_file.py
@@ -16,3 +16,22 @@ def rec_fib(n):
         return 1
     else:
         return rec_fib(n-1)+rec_fib(n-2)
+    
+******************************************
+
+def ite_fact(n):
+    fact=1
+    for i in range(2,n+1):
+        fact=fact*i
+    return fact
+
+******************************************
+
+def ite_fib(n):
+    if n==2:
+        return 1
+    else:
+        a,b = 0,1
+        for i in range(n):
+            a,b = b,a+b
+    return a
